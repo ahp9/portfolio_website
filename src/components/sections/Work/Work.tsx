74386b1613fbdf49ff368818ad71a76eea1a5c20
ahp9 <astridur2001@gmail.com>
2026-08-31 16:31:54 +0000

    docs: make the TODO for Work

--- src/components/sections/Work/Work.tsx
+++ src/components/sections/Work/Work.tsx
@@ -6,7 +6,7 @@ function Work() {
   return (
     <SectionLayout id="work" title="Work" number="02" size="sm" below={ 
       <div className="work__container">
-        <p>Still have to create the Work Section</p>
+        <p>TODO: Still have to create the Work Section</p>
       </div>
     } >
         <p></p>
